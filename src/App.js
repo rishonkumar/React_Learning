2f3ed6da2c6aa6edc77e7316ac2baab64e71f0c8
rishonkumar <rishonkumarrahi@gmail.com>
2022-06-14 10:06:24 +0530

    Class component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-const name = <span>Hello react</span> 
+// const name = <span>Hello react</span> 
 
-const test = () => {
-  alert("Hello heyyy!!!")
-}
+// const test = () => {
+//   alert("Hello heyyy!!!")
+// }
 
-const age = (a) => {
+// const age = (a) => {
 
-  if(a > 18) {
-    return <span>You are elegible </span>
+//   if(a > 18) {
+//     return <span>You are elegible </span>
+//   }
+//   else {l̥
+//     return <span>You are not elegible </span>
+//   }
+// }
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1 className='h1Tag'>Rishon {name}</h1>
+//       <button onClick={test}>Click me</button>
+//       {age(19)}
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  render() {
+    return <h1>Hello class component</h1>
   }
-  else {
-    return <span>You are not elegible </span>
-  }
-}
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <h1 className='h1Tag'>Rishon {name}</h1>
-      <button onClick={test}>Click me</button>
-      {age(19)}
-    </div>
-  );
 }
 
 export default App;
