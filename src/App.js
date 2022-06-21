8f4372182d9e34e03e0e3c64770c184475c42252
rishonkumar <rishonkumarrahi@gmail.com>
2022-06-21 18:09:50 +0530

    Use state with array

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ import Product from './components/Product';
 import HookCounter from './components/HookCounter';
 import HookCounter2 from './components/HookCounter2';
 import HookCounter3 from './components/HookCounter3';
+import HookCounter4 from './components/HookCounter4';
 // import CHeader from './components/CHeder';
+
 
 // const name = <span>Hello react</span> 
 
@@ -76,8 +78,8 @@ class App extends React.Component{
         {/* <HookCounter></HookCounter> */}
         {/* <HookCounter2></HookCounter2> */}
 
-        <HookCounter3></HookCounter3>
-
+        {/* <HookCounter3></HookCounter3> */}
+        <HookCounter4/>
       </div>
     )
   }
